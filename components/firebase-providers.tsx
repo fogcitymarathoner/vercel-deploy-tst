e99c4f7f118a9d60fe344e52dc0f4c79c8c8ef11
marc condon <marc@fogtest.com>
2024-08-26 20:31:24 -0700

    Moved credentials into the system enviroment to be accessed in components/firebase-providers.tsx

---
+++ components/firebase-providers.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { FC, ReactNode, useMemo } from "react";
+import {
+  AnalyticsProvider,
+  AuthProvider,
+  FirebaseAppProvider,
+  FirestoreProvider,
+  useFirebaseApp,
+} from "reactfire";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { isBrowser } from "@/lib/utils";
+import { getAnalytics } from "firebase/analytics";
+import { FirebaseOptions } from "firebase/app";
+
+const config: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId
+};
+
+const FirebaseProviderSDKs: FC<{ children: ReactNode }> = ({ children }) => {
+  const firebase = useFirebaseApp();
+  // we have to use getters to pass to providers, children should use hooks
+  const auth = useMemo(() => getAuth(), []);
+  const firestore = useMemo(() => getFirestore(firebase), []);
+  const analytics = useMemo(() => isBrowser() && getAnalytics(firebase), []);
+
+  return (
+    <>
+      {auth && (
+        <AuthProvider sdk={auth}>
+          <FirestoreProvider sdk={firestore}>
+            {/* we can only use analytics in the browser */}
+            {analytics ? (
+              <AnalyticsProvider sdk={analytics}>{children}</AnalyticsProvider>
+            ) : (
+              <>{children}</>
+            )}
+          </FirestoreProvider>
+        </AuthProvider>
+      )}
+    </>
+  );
+};
+
+export const MyFirebaseProvider: FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <>
+      <FirebaseAppProvider firebaseConfig={config}>
+        <FirebaseProviderSDKs>{children}</FirebaseProviderSDKs>
+      </FirebaseAppProvider>
+    </>
+  );
+};
